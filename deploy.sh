9014d7331e3633d6c5bd455bd9a7176c3721668a
spybond08 <spybond08@gmail.com>
2018-03-19 15:43:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
-git clone https://github.com/Artemmkin/reddit.git
+# Prepare to deploy
+sudo apt update
+sudp apt install git -y
+
+# Deploy app
+cd $HOME
+if [ -d $HOME/reddit ]; then
+    rm -rf $HOME/reddit && git clone https://github.com/Otus-DevOps-2017-11/reddit.git; else
+    git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+fi
+
 cd reddit && bundle install
 puma -d
+ps aux | grep puma
